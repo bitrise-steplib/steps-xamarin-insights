998405eaac7bf366f69af388fdca547d9c2d4f08
birmacher <birmacher@gmail.com>
2016-02-16 11:17:18 +0100

    1.0.0

--- step.sh
+++ step.sh
@@ -12,21 +12,24 @@ if [ -z "$dsym_path" ]; then
 	exit 1
 fi
 
+shopt -s nocasematch
 
 zip_dsym_path=""
-zip_ext=".zip"
-
-if [[ "${dsym_path}" == *${zip_ext} ]] ; then
-
-	zip_dsym_path="${dsym_path}"
-
-else
+if [[ -d "${dsym_path}" ]] ; then
 	printf "\e[34mZipping dSYM...\e[0m\n"
-
-	zip_dsym_path="${dsym_path}${zip_ext}"
-
+	zip_dsym_path="${dsym_path}.zip"
 	zip -r "${zip_dsym_path}" "${dsym_path}"
+elif [[ -f "${dsym_path}" ]]; then
+	if [[ $dsym_path =~ \.zip$ ]] ; then
+		zip_dsym_path="${dsym_path}"
+	else
+		printf "\e[31mError: \e[0mUnsupported dSYM format\n"
+		exit 1
+	fi
+else
+	printf "\e[31mError: \e[0mFile not found at path ${dsym_path}\n"
+	exit 1
 fi
 
-printf "\e[34mUploading dSYM to Xamarin.Insights\e[0m\n"
+printf "\e[34mUploading ${zip_dsym_path} to Xamarin.Insights\e[0m\n"
 curl -w "Upload returned: %{http_code}\\n" -sfF "dsym=@${zip_dsym_path};type=application/zip" "https://xaapi.xamarin.com/api/dsym?apikey=${xamarin_insights_api_key}" -o /dev/null
